f638aa4ed5c71a43635ed1cbc4b7ad4542fe0fa5
renatovieiradesouza <renatovieiradesouza1@gmail.com>
2019-12-23 14:37:27 -0300

    Ajustando pemrissões s3 bucket

--- 03-interpolacao/main.tf
+++ 03-interpolacao/main.tf
@@ -6,6 +6,7 @@ provider "aws" {
 # Bucket test
 resource "aws_s3_bucket" "b" {
   bucket = "my-tf-test-bucket-20122019192932"
+  force_destroy = true
 
   tags = {
     Name        = "My bucket"
@@ -13,23 +14,25 @@ resource "aws_s3_bucket" "b" {
   }
 }
 
+# Acesso público ao bucket
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.b.id
-
   block_public_acls   = false
   block_public_policy = false
 }
 
 # Upload 1  -file txt com interpolation na versão 0.12
 resource "aws_s3_bucket_object" "object" {
+  force_destroy = true
   bucket = aws_s3_bucket.b.id # Estamos pegando o id do bucket na AWS, para isso referenciamos o nome do bucket
   key    = "hello_world63.txt" # Nome do arquivo no bucket
   source = "teste.txt"
-  acl    = "public-read"
+  acl    = "public-read" # Acesso publico para leitura, lista de permissões AWS S3 -> https://docs.aws.amazon.com/AmazonS3/latest/dev/acl-overview.html#canned-acl
 }
 
 # Upload 2 - file txt com interpolation na versão 0.11
 resource "aws_s3_bucket_object" "object2" {
+  force_destroy = true
   bucket = "${aws_s3_bucket.b.id}" # Estamos pegando o id do bucket na AWS, para isso referenciamos o nome do bucket
   key    = "hello_world2.txt" # Nome do arquivo no bucket
   source = "teste.txt"
