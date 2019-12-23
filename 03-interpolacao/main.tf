eee404c46d8bdcc1172a809014d0ad62e194cffe
renatovieiradesouza <renatovieiradesouza1@gmail.com>
2019-12-23 10:25:06 -0300

    Adicionando permissão public-read num objeto no S3, aula 03

--- 03-interpolacao/main.tf
+++ 03-interpolacao/main.tf
@@ -6,7 +6,6 @@ provider "aws" {
 # Bucket test
 resource "aws_s3_bucket" "b" {
   bucket = "my-tf-test-bucket-20122019192932"
-  acl    = "private"
 
   tags = {
     Name        = "My bucket"
@@ -14,14 +13,22 @@ resource "aws_s3_bucket" "b" {
   }
 }
 
-# Upload 1  -file txt com interpolation na versão 0.11
-resource "aws_s3_bucket_object" "object" {
-  bucket = aws_s3_bucket.b.id # "${aws_s3_bucket.b.id}" # Estamos pegando o id do bucket na AWS, para isso referenciamos o nome do bucket
-  key    = "hello_world63.txt" # Nome do arquivo no bucket
-  source = "teste.txt"
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.b.id
+
+  block_public_acls   = false
+  block_public_policy = false
 }
 
-# Upload 2 - file txt com interpolation na versão 0.12
+# Upload 1  -file txt com interpolation na versão 0.12
+resource "aws_s3_bucket_object" "object" {
+  bucket = aws_s3_bucket.b.id # Estamos pegando o id do bucket na AWS, para isso referenciamos o nome do bucket
+  key    = "hello_world63.txt" # Nome do arquivo no bucket
+  source = "teste.txt"
+  acl    = "public-read"
+}
+
+# Upload 2 - file txt com interpolation na versão 0.11
 resource "aws_s3_bucket_object" "object2" {
   bucket = "${aws_s3_bucket.b.id}" # Estamos pegando o id do bucket na AWS, para isso referenciamos o nome do bucket
   key    = "hello_world2.txt" # Nome do arquivo no bucket
